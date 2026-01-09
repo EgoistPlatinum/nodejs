fc0f61b6a1889d9688bf873f69851b1ff181839e
Aleksey Leonov <74103840+AlekseyLeonov031@users.noreply.github.com>
2026-01-09 18:14:40 +0500

    Introduce express server and nunjucks templating, update dependencies for server setup

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const generateId = require("./id");
+const express = require("express");
+const nunjucks = require("nunjucks");
 
-console.log(generateId);
-console.log("NODE_ENV", process.env.NODE_ENV === "production");
+const app = express();
+const port = 3000;
+const uikitCss =
+  '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.25.4/dist/css/uikit.min.css" />';
+
+let count = 0;
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+// ejs
+// app.set("view engine", "ejs")
+
+// njk
+app.set("view engine", "njk");
+
+app.post("/inc", (req, res) => {
+  count++;
+  res.json({ count });
+});
+
+// ejs
+// app.get('/', (req, res) => {
+//   res.render('index', {count, uikitCss})
+// });
+
+// njk
+app.get("/", (req, res) => {
+  const counts = [];
+  for (let i = 0; i < count; i++) {
+    counts.push(99 - i);
+  }
+  res.render("index", { count, uikitCss, counts });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
